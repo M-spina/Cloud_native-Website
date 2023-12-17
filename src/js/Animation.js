024ec81f1e06ac1c2b9f2e023180fa15f52ffca8
Malcolm Spina <malpoke1@gmail.com>
2023-12-17 19:22:07 +0000

    addd some simple animation to nav
    I add a simple animation to the logo in the nave bar

--- src/js/Animation.js
+++ src/js/Animation.js
@@ -12,3 +12,30 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+const text = document.querySelector('.ani');
+const strText = text.textContent;
+const splitText = strText.split("");
+
+text.textContent = "";
+
+for(let i=0; i<splitText.length; i++){
+    text.innerHTML += "<span>"+ splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick,50);
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('asad');
+    char++;
+    if(char === splitText.length){
+        complete();
+        return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
